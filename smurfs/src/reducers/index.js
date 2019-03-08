31f96185fb84bd8309c6924c7bc8e62fa653b31c
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-03-08 11:42:59 -0500

    Set up CSS the way I like it. Fleshed out reducer to handle fetch (R), add (C), edit (U), and delete (D).

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -2,6 +2,21 @@
   Be sure to import in all of the action types from `../actions`
 */
 
+import {
+  FETCH_SMURFS_REQUEST,
+  FETCH_SMURFS_SUCCESS,
+  FETCH_SMURFS_FAILURE,
+  ADD_SMURF_REQUEST,
+  ADD_SMURF_SUCCESS,
+  ADD_SMURF_FAILURE,
+  EDIT_SMURF_REQUEST,
+  EDIT_SMURF_SUCCESS,
+  EDIT_SMURF_FAILURE,
+  DELETE_SMURF_REQUEST,
+  DELETE_SMURF_SUCCESS,
+  DELETE_SMURF_FAILURE
+} from '../actions';
+
 /*
  Your initial/default state for this project could *Although does not have to* look a lot like this
  {
@@ -18,7 +33,7 @@ const initialState = {
   smurfs: [],
   fetchingSmurfs: false,
   addingSmurf: false,
-  updatingSmurf: false,
+  editingSmurf: false,
   deletingSmurf: false,
   error: null
 };
@@ -33,6 +48,54 @@ const initialState = {
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case FETCH_SMURFS_REQUEST:
+      return { ...state, fetchingSmurfs: true, error: null };
+    case FETCH_SMURFS_SUCCESS:
+      return {
+        ...state,
+        fetchingSmurfs: false,
+        smurfs: action.payload,
+        error: null
+      };
+    case FETCH_SMURFS_FAILURE:
+      return { ...state, fetchingSmurfs: false, error: action.payload };
+
+    case ADD_SMURF_REQUEST:
+      return { ...state, addingSmurf: true, error: null };
+    case ADD_SMURF_SUCCESS:
+      return {
+        ...state,
+        addingSmurf: false,
+        smurfs: action.payload,
+        error: null
+      };
+    case ADD_SMURF_FAILURE:
+      return { ...state, addingSmurf: false, error: action.payload };
+
+    case EDIT_SMURF_REQUEST:
+      return { ...state, editingSmurf: true, error: null };
+    case EDIT_SMURF_SUCCESS:
+      return {
+        ...state,
+        editingSmurf: false,
+        smurfs: action.payload,
+        error: null
+      };
+    case EDIT_SMURF_FAILURE:
+      return { ...state, editingSmurf: false, error: action.payload };
+
+    case DELETE_SMURF_REQUEST:
+      return { ...state, deletingSmurf: true, error: null };
+    case DELETE_SMURF_SUCCESS:
+      return {
+        ...state,
+        deletingSmurf: false,
+        smurfs: action.payload,
+        error: null
+      };
+    case DELETE_SMURF_FAILURE:
+      return { ...state, deletingSmurf: false, error: action.payload };
+
     default:
       return state;
   }
